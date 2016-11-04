7f0975e6d809682021e8a6fed50d1fa650aa9fac
Amy Hwang <amyhwang1218@gmail.com>
2016-11-04 10:53:17 -0700

    connect pizza UI anad BL

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@
 function Pizza(size, sauce, meat, nonMeat, address) {
   this.size = size;
   this.sauce = sauce;
-  this.meat = [];
-  this.nonMeat = [];
+  this.meates = [];
+  this.nonMeates = [];
 };
+
 function Address(first, last, street, city, state, zipcode) {
   this.fistName = first;
   this.lastName = last;
@@ -18,18 +19,21 @@ Address.prototype.fullName = function() {
 
 
 $(document).ready(function() {
-  $("form.size").submit(function(event) {
+  $("form.pizza").submit(function(event) {
     event.preventDefault();
 
     var size =  $("input:radio[name=size]:checked").val();
     var sauce = $("input:radio[name=sauce]:checked").val();
+    var newPizza = new Pizza(size, sauce)
     $("input:checkbox[name=meat]:checked").each(function(){
       var meat = $(this).val();
-      this.meat.push(meat);
+      newPizza.meates.push(meat);
     });
     $("input:checkbox[name=non-meat]:checked").each(function(){
       var nonMeat = $(this).val();
-      this.nonMeat.push(nonMeat);
+      newPizza.nonMeates.push(nonMeat);
     });
+
+    console.log(newPizza)
   });
 });
