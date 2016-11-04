20e50e7bf06094650170ed284fd38d0a1790e520
Amy Hwang <amyhwang1218@gmail.com>
2016-11-04 16:24:33 -0700

    fix confimation section

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,130 @@
 //business logic
-function Info(pizza, address) {
-  this.pizza = pizza;
-  this.address = address;
-}
-
-function Pizza(size, sauce, meat, nonMeat, address) {
-  this.size = size;
+function Pizza(name, size, sauce, meat, nonMeat, address) {
+  this.pizzaName = name
+  this.size = parseInt(size);
   this.sauce = sauce;
   this.meats = [];
   this.nonMeats = [];
 };
 
+Pizza.prototype.cost = function() {
+  return this.size + (3 * parseInt(this.meats.length)) + (2 * parseInt(this.nonMeats.length))
+}
+
 function Address(first, last, street, city, state, zipcode) {
-  this.fistName = first;
+  this.firstName = first;
   this.lastName = last;
-  this.fullAddress = [street, city, state, zipcode];
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zipcode = zipcode;
 };
 
 Address.prototype.fullName = function() {
-  return this.firstName + " " + this.Lastname;
+  return this.firstName + " " + this.lastName;
 }
 
-Pizza.prototype.toppings = function() {
-  return this.meats + "," + this.nonMeats
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + ", " + this.zipcode;
 }
+
 
 
 $(document).ready(function() {
+  //move on sauce
   $("h4#size-submit").click(function() {
       $(".size").slideUp();
       $(".sauce").slideDown();
   });
 
+  //go back to size
   $("h4#size-edit").click(function() {
     $(".sauce").slideUp();
     $(".size").slideDown();
   });
 
+  //move on to topping
   $("h4#sauce-submit").click(function() {
       $(".sauce").slideUp();
       $(".topping").slideDown();
   });
 
+  //go back to sauce
   $("h4#sauce-edit").click(function() {
-    $(".size").hide();
     $(".topping").slideUp();
     $(".sauce").slideDown();
   });
 
+  //move on to pizza naming
+  $("h4#topping-submit").click(function() {
+    $(".topping").slideUp();
+    $(".pizza-name").slideDown();
+  });
+
+  //go back to topping
+  $("h4#topping-edit").click(function() {
+    $(".pizza-name").slideUp();
+    $(".topping").slideDown();
+  });
+
+  //done with pizza, move on to address
   $("h4#pizza-edit").click(function() {
     $(".size").show();
     $("form.address").slideUp();
     $("form.pizza").slideDown();
   });
 
+  //when pizza submit button is clicked
   $("form.pizza").submit(function(event) {
     event.preventDefault();
     var btn = $(this).find("button#pizza-submit[type=submit]:focus" );
 
     var size =  $("input:radio[name=size]:checked").val();
     var sauce = $("input:radio[name=sauce]:checked").val();
+    var name = $("input#pname").val();
 
-    var newPizza = new Pizza(size, sauce);
+    //setting vars for pizza object properties
+    var newPizza = new Pizza(name, size, sauce);
+
+
     $("input:checkbox[name=meat]:checked").each(function(){
       var meat = $(this).val();
+      console.log(meat)
       newPizza.meats.push(meat);
+      console.log(newPizza.meats);
+      console.log(newPizza.meats.length)
     });
+
     $("input:checkbox[name=non-meat]:checked").each(function(){
       var nonMeat = $(this).val();
       newPizza.nonMeats.push(nonMeat);
     });
 
+    //pizza done, move one to address
     $("form.pizza").slideUp();
     $("form.address").slideDown();
 
-    console.log(newPizza);
+    //output pizza names
+    $("ul#pizza-list").append("<li><span class='pizza-names'>" + newPizza.pizzaName + "</span></li>");
+
+    //change the size values from number to strings
+    var sizeName = function() {
+      if (size === "7") {
+        return "Small"
+      } else if (size === "10") {
+        return "Medium"
+      } else if (size === "13") {
+        return "large"
+      }
+    }
+
+    //output
+    $(".size-confirm").text(sizeName);
+    $(".sauce-confirm").text(newPizza.sauce);
+    $(".meat-confirm").append("<li>" + newPizza.meats + "</li>");
+    $(".non-meat-confirm").append("<li>" + newPizza.nonMeats + "</li>");
+    $(".cost-confirm").text(newPizza.cost());
+
   });
 
 
@@ -86,19 +136,31 @@ $(document).ready(function() {
     var first =  $("input#FirstName").val();
     var last = $("input#LastName").val();
     var street = $("input#StreetAddress").val();
-    var city = $("input#city").val();
+    var city = $("input#City").val();
     var state = $("select#State").val();
+    var zipcode = $("input#ZipCode").val();
 
-    var newAddress= new Address(first, last, street, city, state);
+    var newAddress= new Address(first, last, street, city, state, zipcode);
 
     console.log(newAddress);
 
     $("form.address").slideUp();
 
-    $("#confirmation").show();
-    $("#confirmation").text(newContact.firstName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
+    $(".pizza-info").show();
+    $(".pizza-names").last().click(function() {
+      $(".confirmation").show();
+      $(".name-confirm").text(newAddress.fullName());
+      $(".address-confirm").text(newAddress.fullAddress());
+
+      $("h4#edit").click(function() {
+        $("input#pname").val("");
+        $("form.pizza").show();
+        $(".pizza-name").hide();
+        $(".size").slideDown();
+        $(".pizza-info").slideUp();
+        $(".confirmation").slideUp();
+      });
+    });
 
   });
 });
